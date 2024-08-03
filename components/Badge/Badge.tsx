2359135adff9bbf86e2a8dc1dc35266d6ace3e2f
Brian <brian@brianaderer.com>
2024-08-03 10:53:46 -0400

    adjust the badge output

--- components/Badge/Badge.tsx
+++ components/Badge/Badge.tsx
@@ -9,8 +9,8 @@ const Badge: FC<{ contact: Contact, visible: boolean }> = ({ contact, visible })
         <div className={`flex flex-col gap-4 text-xl overflow-hidden transition-all items-end border-b border-l border-green-600 p-10 ${visible ? 'opacity-1' : 'opacity-0 -z-10 h-0'}`}>
             <MenuLink selectable={false}>{name}</MenuLink>
             {/*<p>{phoneNumber}</p>*/}
-            <MenuLink>{email}</MenuLink>
-            <MenuLink>{website}</MenuLink>
+            <MenuLink href={`mailto: ${email}`}>{email}</MenuLink>
+            <MenuLink href={`https://www.${website}`}>{website}</MenuLink>
         </div>
     );
 };
--- components/MenuLink/MenuLink.tsx
+++ components/MenuLink/MenuLink.tsx
@@ -1,10 +1,14 @@
 import { FC } from "react";
 import {ChildrenProps} from "@/interfaces/ChildrenProps";
 
-const MenuLink: FC<ChildrenProps>  = props => {
-    const {children, onClick, className='', selectable = true} = props;
+interface MenuLinkProps extends ChildrenProps {
+    href?: string | false;
+}
+
+const MenuLink: FC<MenuLinkProps>  = props => {
+    const {href = false, children, onClick, className='', selectable = true} = props;
     return (
-        <a className={`select-none border-transparent border-b-2 ${selectable && `cursor-pointer hover:border-white`} ${className}`} onClick={onClick}>
+        <a {...(href ? { href } : {})} target={'_blank'} className={`select-none border-transparent border-b-2 ${selectable && `cursor-pointer hover:border-white`} ${className}`} onClick={onClick}>
             {children}
         </a>
     )
