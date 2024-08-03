48ea03701e34dcf92fdb4124880ee522eb4a2721
Brian <brian@brianaderer.com>
2024-08-03 16:42:14 -0400

    add logging

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import {HomeProps, Technology, StringArray, Headline} from '@/interfaces';
 
 export async function getStaticProps() {
     const client = createApolloClient();
+    console.log('Fetching data...');
     const { data } = await client.query({
         query: gql`
         query MyQuery {
@@ -100,7 +101,7 @@ export async function getStaticProps() {
         }
         `,
     });
-
+    console.log('Data fetched successfully:', result.data);
     return {
         props: {
             contacts: data.listContact.edges,
